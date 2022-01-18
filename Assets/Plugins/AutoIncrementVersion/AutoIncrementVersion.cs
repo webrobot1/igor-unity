fa83d55fd6453a5eac7a18d001040fbcc2f3d4cb
Срельцов Михаил <admin@my-fantasy.ru>
2022-01-18 17:09:54 +0300

    no message

--- Assets/Plugins/AutoIncrementVersion/AutoIncrementVersion.cs
+++ Assets/Plugins/AutoIncrementVersion/AutoIncrementVersion.cs
@@ -4,10 +4,8 @@ using System;
 
 public class AutoIncrementVersion : MonoBehaviour
 {
-#if UNITY_CLOUD_BUILD
     public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
     {
         PlayerSettings.bundleVersion = String.Format(PlayerSettings.bundleVersion + ".{0}", manifest.GetValue("buildNumber", "unknown"));
     }
-#endif
 }
