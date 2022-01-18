e6654ccaa2ce25a044ab8e55ce26009d6fee30dd
Срельцов Михаил <admin@my-fantasy.ru>
2022-01-18 16:41:34 +0300

    no message

--- Assets/Plugins/AutoIncrementVersion/AutoIncrementVersion.cs
+++ Assets/Plugins/AutoIncrementVersion/AutoIncrementVersion.cs
@@ -1,26 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEditor;
-using UnityEngine;
 
-// https://bigcheeseapp.com/2019/04/25/automatically-set-ios-build-number-and-android-version-code-in-unity-cloud-build/
 public class AutoIncrementVersion
 {
 #if UNITY_CLOUD_BUILD
     public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
     {
-        // to update major or minor version, manually set it in Edit>Project Settings>Player>Other Settings>Version
-
-        // this will also set Application.version which is readonly
-        int[] versionParts = PlayerSettings.bundleVersion.Split('.');
-        if (versionParts.Length != 3 || versionParts[2].ParseInt(-1) == -1)
-        {
-            Debug.LogError("BuildPostprocessor failed to update version " + PlayerSettings.bundleVersion);
-            return;
-        }
-        // major-minor-build
-        versionParts[2] = (versionParts[2].ParseInt() + 1).ToString();
-        PlayerSettings.bundleVersion = versionParts.Join(".");
+        PlayerSettings.bundleVersion = String.Format(PlayerSettings.bundleVersion + ".{0}", manifest.GetValue("buildNumber", "unknown"));
     }
 #endif
 }
