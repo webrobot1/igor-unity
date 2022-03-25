afe4be71dc8454ee496a481ee58e474b203ec440
Streltsov Mikhail <admin@my-fantasy.ru>
2022-03-26 00:10:17 +0300

    no message

--- Assets/Scripts/RegisterController.cs
+++ Assets/Scripts/RegisterController.cs
@@ -34,7 +34,7 @@ public class RegisterController : MainController
         formData.AddField("login", login.text);
         formData.AddField("password", password.text);
 
-        UnityWebRequest request = UnityWebRequest.Post("http://95.216.204.181:8080/api/signin/" + action, formData);
+        UnityWebRequest request = UnityWebRequest.Post("http://194.67.205.140/api/signin/" + action, formData);
 
         yield return request.SendWebRequest();
 
