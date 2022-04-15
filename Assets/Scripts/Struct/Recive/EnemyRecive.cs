046747c5197532e8854b3cba662cb02d084f2ea8
Стрельцов Михаил <admin@my-fantasy.ru>
2022-04-15 13:50:27 +0300

    no message

--- Assets/Scripts/Struct/Recive/EnemyRecive.cs
+++ Assets/Scripts/Struct/Recive/EnemyRecive.cs
@@ -5,6 +5,7 @@
 [System.Serializable]
 public class EnemyRecive : ObjectRecive
 {
+	// hp без знака ? всегда не пришедшее будет = 0 те будто надо дергать лайфба а с ним по умолчанию null и мы првоеряем именно пришло ли что то
 	public int? hp;
 	public int hpMax;
 
