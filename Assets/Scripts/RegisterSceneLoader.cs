c213b224c8c72b876c12616892e61ba154eb4499
Стрельцов Михаил <admin@my-fantasy.ru>
2024-03-08 23:11:08 +0300

    no message

---
+++ Assets/Scripts/RegisterSceneLoader.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+/// <summary>
+/// При разработке в Unity в Play Mode (режим отладки) автоматически активирует сцену регистрации (так что при разработке можно постоянно держать открыто MainScene). 
+/// А при сборке регистрация стоит перовой сценой и неважно какая была открыты в Unity
+/// </summary>
+[InitializeOnLoadAttribute]
+public static class RegisterSceneLoader
+{
+	static RegisterSceneLoader()
+	{
+		EditorApplication.playModeStateChanged += LoadDefaultScene;
+	}
+
+	static void LoadDefaultScene(PlayModeStateChange state)
+	{
+		if (state == PlayModeStateChange.ExitingEditMode)
+		{
+			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+		}
+
+		if (state == PlayModeStateChange.EnteredPlayMode)
+		{
+			EditorSceneManager.LoadScene(0);
+		}
+	}
+}
+#endif
